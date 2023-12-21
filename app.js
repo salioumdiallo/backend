bff4c9f2556a5cc8affc683d5a42084ad6259eb5
Mamadou saliou diallo <salioumdiallo@gmail.com>
2023-12-21 11:11:56 +0100

    version 3

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose =require("mongoose");
 const dotenv =require('dotenv');
 const categorieRouter =require("./routes/categorie.route")
 const scategorieRouter =require("./routes/scategorie.route")
+const articleRouter =require("./routes/article.route")
 const app = express();
 dotenv.config()
 app.use(express.json());
@@ -24,5 +25,6 @@ res.send("Bibliothèque");
 });
 app.use('/api/categories', categorieRouter);
 app.use('/api/scategories', scategorieRouter);
+app.use('/api/articles', articleRouter);
 app.listen(process.env.PORT, () => {
 console.log(`Server is listening on port ${process.env.PORT}`); });
