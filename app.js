be2a4f1c43a78507adf73c5f40f7e23adaf72d15
Mamadou saliou diallo <salioumdiallo@gmail.com>
2023-12-21 09:57:30 +0100

    version 0

---
+++ app.js
@@ -0,0 +1,26 @@
+const express =require('express');
+const mongoose =require("mongoose");
+const dotenv =require('dotenv');
+const categorieRouter =require("./routes/categorie.route")
+const app = express();
+dotenv.config()
+app.use(express.json());
+
+// Connexion à la base données
+mongoose.connect(process.env.DATABASE,{
+useNewUrlParser: true,
+useUnifiedTopology: true
+})
+.then(() => {console.log("Connexion à la base de données réussie");
+}).catch(err => {
+console.log('Impossible de se connecter à la base de données',
+err);
+process.exit();
+});
+
+app.get("/",(req,res)=>{
+res.send("Bibliothèque");
+});
+app.use('/api/categories', categorieRouter);
+app.listen(process.env.PORT, () => {
+console.log(`Server is listening on port ${process.env.PORT}`); });
